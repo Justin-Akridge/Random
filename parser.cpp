4b47943c0b3da5f0c99df6eb5fb61ba175acf769
Justin Akridge <Akridjus@gmail.com>
2023-08-12 17:48:12 -0400

    expression function

--- parser.cpp
+++ parser.cpp
@@ -1,39 +1,22 @@
 #include <iostream>
-#include <string>
-using namespace std;
 
 
-void error(string &s) {
-  std::cerr << s << std::endl;
+Token get_token();
+double expression() {
+  double left = expression();
+  Token t = get_token();
+  switch(t.kind) {
+    case '+':
+      return left + expression();
+    case '-':
+      return left - expression();
+    default:
+      return expression();
+  }
 }
+double term();
+double primary();
+
 
 int main() {
-    cout << "Please enter expression: ";
-    int lval = 0;
-    int rval;
-    int res;
-    cin >> lval 
-    if (!cin) error ("no first operand");
-    for (char op; cin >> op) {
-    if (op != 'x') cin >> rval;
-    if (!cin) error ("no second operand");
-    switch(op) {
-      case '+':
-        lval += rval;
-        break;
-      case '-':
-        lval -= rval;
-        break;
-      case '*':
-        lval *= rval;
-        break;
-      case '/':
-        lval /= rval;
-        break;
-      default:
-        cout << "RESULT = "
-    }
-
-    cout << "RESULT: " << res << '\n';
-    return 0;
 }
