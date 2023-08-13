d4a12559b7d3c155b12d54859630796401b535f2
Justin Akridge <Akridjus@gmail.com>
2023-08-12 22:19:23 -0400

    changes

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -6,6 +6,47 @@ double primary();
 double term();
 double expression();
 
+class Token_stream {
+public:
+  Token_stream();
+  Token get();
+  Token putback(Token t);
+private:
+  bool full;
+  Token buffer;
+};
+
+Token_stream::Token_stream()
+  :full(false), buffer(0) {}
+
+Token Token_stream::get() {
+  if (full) {
+    full = false;
+    return buffer;
+  }
+  char ch;
+  std::cin >> ch;
+  case ';':
+  case 'q':
+  case '(': case ')': case '+': case '-': case '/': case '*': case '%':
+    return Token(ch);
+  case '.':
+  case '0': case '1': case '2': case '3': case '4': 
+  case '5': case '6': case '7': case '8': case '9': 
+  {
+    std::cin.putback(ch);
+    double val;
+    std::cin >> val;
+    return Token('8', val);
+  }
+}
+
+void Token_stream::putback(Token t) {
+  if (full) error("putback() into a full buffer");
+  buffer = t;
+  full = true;
+}
+
 class Token {
 public:
   char kind;
