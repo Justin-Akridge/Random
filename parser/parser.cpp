061e72b6a94d51ec0b904cefe17a1d2f1562c643
Justin Akridge <Akridjus@gmail.com>
2023-08-12 20:43:59 -0400

    refactor

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -8,109 +8,100 @@ double expression();
 
 class Token {
 public:
-    char kind;        // what kind of token
-    double value;     // for numbers: a value 
-    Token(char ch)    // make a Token from a char
-        :kind(ch), value(0) { }
-    Token(char ch, double val)     // make a Token from a char and a double
-        :kind(ch), value(val) { }
+  char kind;
+  double value;
+  Token(char ch)
+    :kind(ch), value(0) { }
+  Token(char ch, double val)
+    :kind(ch), value(val) { }
 };
 
-//------------------------------------------------------------------------------
-
 Token get_token() {
-    char ch;
-    std::cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
-
-    switch (ch) {
-        //not yet   case ';':    // for "print"
-        //not yet   case 'q':    // for "quit"
+  char ch;
+  std::cin >> ch;
+  switch (ch) {
     case '(': case ')': case '+': case '-': case '*': case '/':
-        return Token(ch);        // let each character represent itself
+      return Token(ch);
     case '.':
     case '0': case '1': case '2': case '3': case '4':
-    case '5': case '6': case '7': case '8': case '9':
-    {
-      std::cin.putback(ch);         // put digit back into the input stream
-        double val;
-        std::cin >> val;              // read a floating-point number
-        return Token('8', val);   // let '8' represent "a number"
+    case '5': case '6': case '7': case '8': case '9': {
+      std::cin.putback(ch);
+      double val;
+      std::cin >> val;
+      return Token('8', val);
     }
     default:
-        error("Bad token");
-    }
+      error("Bad token");
+  }
 }
 
-                      
 void error(std::string s) {
   std::cerr << s << std::endl;
 }
 
 double primary() {
-    Token t = get_token();
-    switch (t.kind) {
-    case '(':    // handle '(' expression ')'
-    {
-        double d = expression();
-        t = get_token();
-        if (t.kind != ')') error("')' expected");
-        return d;
-    }
-    case '8':            // we use '8' to represent a number
-        return t.value;  // return the number's value
-    default:
-        error("primary expected");
-    }
+  Token t = get_token();
+  switch (t.kind) {
+  case '(': {
+    double d = expression();
+    t = get_token();
+    if (t.kind != ')') error("')' expected");
+    return d;
+  }
+  case '8':
+    return t.value;
+  default:
+    error("primary expected");
+  }
 }
 
 double term() {
-    double left = primary();
-    Token t = get_token();     // get the next token
-
-    while (true) {
-        switch (t.kind) {
-        case '*':
-            left *= primary();
-            t = get_token();
-            break;
-        case '/':
-        {
-            double d = primary();
-            if (d == 0) error("divide by zero");
-            left /= d;
-            t = get_token();
-            break;
-        }
-        default:
-            return left;
-        }
+  double left = primary();
+  Token t = ts.get();
+  while (true) {
+    switch (t.kind) {
+    case '*':
+      left *= primary();
+      t = ts.get();
+      break;
+    case '/': {
+      double d = primary();
+      if (d == 0) error("divide by zero");
+      left /= d;
+      t = ts.get();
+      break;
     }
+    default:
+      ts.put_back(t);
+      return left;
+    }
+  }
 }
 
 double expression() {
-    double left = term();      // read and evaluate a Term
-    Token t = get_token();     // get the next token
-    while (true) {
-        switch (t.kind) {
-        case '+':
-            left += term();    // evaluate Term and add
-            t = get_token();
-            break;
-        case '-':
-            left -= term();    // evaluate Term and subtract
-            t = get_token();
-            break;
-        default:
-            return left;       // finally: no more + or -: return the answer
-        }
+  double left = term();
+  Token t = ts.get();
+  while (true) {
+    switch (t.kind) {
+    case '+':
+      left += term();
+      t = ts.get();
+      break;
+    case '-':
+      left -= term();
+      t = ts.get();
+      break;
+    default:
+      ts.put_back(t);
+      return left;
     }
+  }
 }
-
 
 int main() {
   try {
-      while (std::cin)
-          std::cout << expression() << '\n';
+    while (std::cin)
+      std::cout << expression() << '\n';
   }
   catch (std::exception& e) {
     std::cerr << e.what() << std::endl;
