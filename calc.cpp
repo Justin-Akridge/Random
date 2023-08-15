5aa4a7763afcdb79812dc706b925dc464ca5824f
Justin Akridge <Akridjus@gmail.com>
2023-08-15 19:52:44 -0400

    refactor

--- calc.cpp
+++ calc.cpp
@@ -17,32 +17,33 @@ public:
 };
 
 Token get_token() {
-    char ch;
-    std::cin >> ch;
-    switch (ch) {
-      case '(': case ')': case '+': case '-': case '*': case '/':
-          return Token(ch);
-      case '.':
-      case '0': case '1': case '2': case '3': case '4':
-      case '5': case '6': case '7': case '8': case '9':
-      {
-          std::cin.putback(ch);
-          double val; std::cin >> val;
-          return Token('8', val);
-      }
-      default:
-          error("bad token");
+  char ch;
+  std::cin >> ch;
+  switch (ch) {
+    case '(': case ')': case '+': case '-': case '*': case '/':
+    {
+      return Token(ch);
     }
+    case '.':
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9':
+    {
+      std::cin.putback(ch);
+      double val; std::cin >> val;
+      return Token('8', val);
+    }
+    default:
+      error("bad token");
+  }
 }
 
 int main() {
   try {
     while (std::cin)
       std::cout << expression() << std::endl;
-    throw 20;
   }
-  catch(int e) {
-    std::cerr << e << '\n';
+  catch(std::exception &e) {
+    std::cerr << e.what() << '\n';
     return 1;
   }
   catch(...) {
@@ -77,19 +78,19 @@ double term() {
   while (true) {
     switch(t.kind) {
       case '*':
-        {
-          left *= primary();
-          t = get_token();
-          break;
-        }
+      {
+        left *= primary();
+        t = get_token();
+        break;
+      }
       case '/':
-        {
-          double d = primary();
-          if (d == 0) error("Cannot divide by 0");
-          left /= primary();
-          t = get_token();
-          break;
-        }
+      {
+        double d = primary();
+        if (d == 0) error("Cannot divide by 0");
+        left /= primary();
+        t = get_token();
+        break;
+      }
       default:
         return left;
     }
