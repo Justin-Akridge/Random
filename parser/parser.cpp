a2827d8bde8d51b60ea5fd40738392474495ad88
Justin Akridge <Akridjus@gmail.com>
2023-08-14 23:03:29 -0400

    refactor

---
+++ parser/parser.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <string>
+
+void error(std::string s);
+double primary();
+double term();
+double expression();
+
+class Token {
+public:
+  char kind;
+  double value;
+  Token(char ch)
+    :kind(ch), value(0) { }
+  Token(char ch, double val)
+    :kind(ch), value(val) { }
+};
+
+void error(std::string s) {
+  std::cerr << s << std::endl;
+}
+
+class Token_stream {
+public:
+  Token_stream();
+  Token get();
+  void putback(Token t);
+private:
+  bool full;
+  Token buffer;
+};
+
+Token_stream::Token_stream()
+  :full(false), buffer(0) {}
+
+Token Token_stream::get() {
+  if (full) {
+    full = false;
+    return buffer;
+  }
+  char ch;
+  std::cin >> ch;
+  switch(ch) {
+    case ';':
+    case 'q':
+    case '(': case ')': case '+': case '-': case '/': case '*': case '%':
+      return Token(ch);
+    case '.':
+    case '0': case '1': case '2': case '3': case '4': 
+    case '5': case '6': case '7': case '8': case '9': 
+    {
+      std::cin.putback(ch);
+      double val;
+      std::cin >> val;
+      return Token('8', val);
+    }
+  }
+}
+
+void Token_stream::putback(Token t) {
+  if (full) error("putback() into a full buffer");
+  buffer = t;
+  full = true;
+}
+
+Token get_token() {
+  char ch;
+  std::cin >> ch;
+  switch (ch) {
+    case '(': case ')': case '+': case '-': case '*': case '/':
+      return Token(ch);
+    case '.':
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9': 
+    {
+      std::cin.putback(ch);
+      double val;
+      std::cin >> val;
+      return Token('8', val);
+    }
+    default:
+      error("Bad token");
+  }
+}
+
+double primary() {
+  Token t = get_token();
+  switch (t.kind) {
+  case '(': {
+    double d = expression();
+    t = get_token();
+    if (t.kind != ')') error("')' expected");
+    return d;
+  }
+  case '8':
+    return t.value;
+  default:
+    error("primary expected");
+  }
+}
+
+double term() {
+  double left = primary();
+
+  Token t = ts.get();
+  while (true) {
+    switch (t.kind) {
+    case '*':
+      left *= primary();
+      t = ts.get();
+      break;
+    case '/': {
+      double d = primary();
+      if (d == 0) error("divide by zero");
+      left /= d;
+      t = ts.get();
+      break;
+    }
+    default:
+      ts.put_back(t);
+      return left;
+    }
+  }
+}
+
+double expression() {
+  double left = term();
+  Token t = ts.get();
+  while (true) {
+    switch (t.kind) {
+    case '+':
+      left += term();
+      t = ts.get();
+      break;
+    case '-':
+      left -= term();
+      t = ts.get();
+      break;
+    default:
+      ts.put_back(t);
+      return left;
+    }
+  }
+}
+
+int main() {
+  try {
+    while (std::cin)
+      std::cout << expression() << '\n';
+  }
+  catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+      return 1;
+  }
+  catch (...) {
+    std::cerr << "exception \n";
+      return 2;
+  }
+}
