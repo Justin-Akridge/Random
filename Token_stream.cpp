5bee41e5f63ce9ef3bbbbad0cb4d5b739c8a2d20
Justin Akridge <Akridjus@gmail.com>
2023-08-15 21:11:08 -0400

    Token stream class implemented

---
+++ Token_stream.cpp
@@ -0,0 +1,39 @@
+class Token_stream {
+public: 
+  Token_stream();
+  Token get();
+  void putback(Token t);
+private:
+  bool full;
+  Token buffer
+};
+
+Token_stream::Token_stream()
+  :full(false), buffer(0) { }
+
+Token_stream::get() {
+  if (full) {
+    full = false;
+    return buffer;
+  }
+
+  char ch;
+  std::cin >> ch;
+  switch(ch) {
+    case ',':
+    case 'q':
+    case '(': case ')': case '-': case '*': case '/': case '%':
+      return Token(ch);
+    case '.':
+    case '0': case '1': case '2': case '3': case '4': 
+    case '5': case '6': case '7': case '8': case '9':
+    {
+      std::cin.putback(ch);
+      double val;
+      std::cin >> val;
+      return Token('8', val);
+    }
+    default:
+      error("bad token");
+  }
+}
